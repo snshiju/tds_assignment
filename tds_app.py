04bac9876cd98103ba958bfbf72b22d6c20f5cf3
snshiju <167642460+snshiju@users.noreply.github.com>
2024-04-21 20:02:51 +0530

    tds_app.py
    modified

--- tds_app.py
+++ tds_app.py
@@ -1,3 +1,19 @@
 import streamlit as st
 
-st.write("Hello world")
+st.subheader("Find Largest Number")
+
+st.write("Enter the numbers:")
+num1 = st.number_input("Number 1",min_value=0,step=1)
+num2 = st.number_input("Number 2",min_value=0,step=1)
+num3 = st.number_input("Number 3",min_value=0,step=1)
+
+ 
+if(num1>num2):
+  largest = num1
+else:
+  largest = num2
+
+if (num3 >largest):
+  largest = num3
+
+st.write("Largest Number: " +largest)
